e4a4dad863acf0c91337737ad3aa016dfa3fcab9
achubbz <anshumanchaube95@gmail.com>
2019-06-11 11:52:35 -0500

    added meshing commands to cmd log
    Former-commit-id: 0a94c290d3dc780822e70048a8495b51a640f726

--- meshes/salt-channel/saltch1-cmd.py
+++ meshes/salt-channel/saltch1-cmd.py
@@ -5,6 +5,28 @@ move Volume 2 x 0.4 include_merged
 Volume 2  copy reflect x group_results
 unite volume 1 2 3
 volume 1 copy scale x 0.8 y 0.7 z 1 nomesh
-surface 20  size 0.05
-surface 20  size 0.05
-mesh surface 20 
+
+intersect volume 1 4 keep 
+subtract volume 4 from volume 1
+
+surface 26 size 0.05
+mesh surface 26
+
+volume 5 redistribute nodes off 
+volume 5 scheme Sweep  source surface 26   target surface 27  sweep transform least squares 
+volume 5 autosmooth target on  fixed imprints off  smart smooth off 
+mesh volume 5
+
+surface 34  size 0.02
+mesh surface 34
+volume 1 redistribute nodes off 
+volume 1 scheme Sweep  source surface 34   target surface 35  sweep transform least squares 
+volume 1 autosmooth target on  fixed imprints off  smart smooth off 
+mesh volume 1
+
+block 100 add volume 1 5
+block 100 element type hex27
+
+set exodus netcdf4 off
+set large exodus file on
+export mesh "~/sneks4me/meshes/salt-channel/salt-channel-scaled.exo"  dimension 3  overwrite 
